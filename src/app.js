7e9d89b55e216a974180429ebf422bd12ec86415
MIsamba <isambagm@gmail.com>
2020-10-12 20:33:09 +0530

    Completed module one nd

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
 const path = require('path');
+
 const express = require('express');
-const app = express(); 
+const app = new express();
+
+app.set('views',path.join(__dirname, '/views'));
+app.set('view engine','ejs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) =>res.render('index', {title: 'index'}));
 
 
-
-var server = app.listen(3000, function () {
-    console.log('PS Project Running on port 3000!');
-});
+app.listen(3000,()=> {console.log('PS Project Running on port 3000!')});
